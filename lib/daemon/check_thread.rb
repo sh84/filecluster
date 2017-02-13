edeb8ca879940e9046976cb36f4932781ea495bf
Андрей Шмелев <a.shmelev@inventos.ru>
2017-02-13 15:01:11 +0300

    add new storage size type: auto

--- lib/daemon/check_thread.rb
+++ lib/daemon/check_thread.rb
@@ -3,6 +3,8 @@ class CheckThread < BaseThread
     $log.debug("CheckThread: Run stotage check for #{storage_name}")
     storage = $storages.detect{|s| s.name == storage_name}
     if File.writable?(storage.path)
+      storage.size_limit = storage.get_real_size if storage.auto_size?
+      # also saves size_limit
       storage.update_check_time
     else 
       error "Storage #{storage.name} with path #{storage.path} not writable"
